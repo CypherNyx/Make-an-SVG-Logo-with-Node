cf4b8632b3f85a79fb1289be258e5116ff4e2d23
Dahlia Guido <dguido.dev@gmail.com>
2023-07-26 17:15:06 -0500

    adding dependencies

---
+++ index.js
@@ -0,0 +1,9 @@
+// import req files and other dependencies
+const inquirer = require ("inquirer");
+const fs = require("fs");
+const catchColor = require("./lib/color");
+const generateShape = require("./lib/shapes");
+const generateSVG = require("./lib/svg");
+
+// Questions prompted by inquirer
+
