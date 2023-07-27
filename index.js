4a7845856f29dedca53dfe0c7fb5532a654caf48
Dahlia Guido <dguido.dev@gmail.com>
2023-07-26 22:32:19 -0500

    CLI prompt questions are working

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ const generateShape = require("./lib/shapes");
 const generateSVG = require("./lib/svg");
 
 // Questions prompted by inquirer
-questions() = [
+const questions = [
 // WHEN I am prompted for text
 // THEN I can enter up to three characters
     {
       type: "input",
-      message: "Enter up to 3 characters to display inside your logo:" ,
       name: "title",
+      message: "Enter up to 3 characters to display inside your logo:" ,
     },
 
 // WHEN I am prompted for the text color
 // THEN I can enter a color keyword (OR a hexadecimal number)
     {
       type: "input",
-      message:"What text color would you like for these characters?",
       name: "textColor",
+      message:"What text color would you like for these characters?",
     },
 
 // WHEN I am prompted for a shape
 // THEN I am presented with a list of shapes to choose from: circle, triangle, and square
     {
       type: "list",
-      message: "Please pick a shape for your logo:",
       name: "shape",
+      message: "Please pick a shape for your logo:",
       choices: ["circle", "triangle", "square"],
     },
 
@@ -36,8 +36,8 @@ questions() = [
 // THEN I can enter a color keyword (OR a hexadecimal number)
     {
       type: "input",
-      message: "What color would you like your shape's background to be?",
       name: 'shapeColor',
+      message: "What color would you like your shape's background to be?",
       // validate: (input) => {
       //   const pickColor = color.includes(input.toLowerCase());
       //   const pickHexColorCode = "";
@@ -45,19 +45,26 @@ questions() = [
       // },
     },
   ];
-
+// Create a function to initialize app
   function runApp(){
     inquirer.prompt(questions)
     .then((answers) => {
-
+      const fileName = "logo.svg";
+      const shapeInfo = generateShape(answers);
+      const svgInfo = generateSVG(answers, shapeInfo);
+      fs.writeFile(fileName, svgInfo);
+     console.log(response);
     })
-
+    .catch((error) => {
+      console.log(error);
+    });
   };
+  // Function call to initialize app
+      runApp();
+      
 
-   
-
-
-
-    console.log(response);
+  
+ 
+    
   
 
